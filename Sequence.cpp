413cfcc4cbb725d2a7224ef16225a223c53829b0
Skatlem <115564126+Skatlem@users.noreply.github.com>
2023-02-01 12:34:51 +0100

    Update Sequence.cpp

--- Sequence.cpp
+++ Sequence.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
 
+Sequence::Sequence(int x, int y){
+    this.x -> x;
+    this.y -> y;
+}
+
+Sequence::Sequence(Sequence kopia){
+    this.x -> kopia.x;
+    this.y -> kopia.y;
+}
+
+Sequence::~Sequence(){
+    delete[] x;
+    delete[] y;
+}
+
+int Sequence::accessX(){
+    return x;
+}
+
+int Sequence::accessY(){
+    return y;
+}
+
+float Sequence::oblicz(){
+    return x+y;
+}
+
+float Sequence::oblicz2(){
+    return x-y;
+}
