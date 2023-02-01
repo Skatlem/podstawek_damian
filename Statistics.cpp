1296dc42925d706632ad080bd361c0f29300b575
Skatlem <115564126+Skatlem@users.noreply.github.com>
2023-02-01 12:50:15 +0100

    class

--- Statistics.cpp
+++ Statistics.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
 
+Statistics::Statistics(int x, int y){
+    this.x -> x;
+    this.y -> y;
+}
+
+Statistics::Statistics(Statistics kopia){
+    this.x -> kopia.x;
+    this.y -> kopia.y;
+}
+
+Statistics::~Statistics(){
+    delete[] x;
+    delete[] y;
+}
+
+int Statistics::accessX(){
+    return x;
+}
+
+int Statistics::accessY(){
+    return y;
+}
+
+float Statistics::oblicz(){
+    return x+y;
+}
+
+float Statistics::oblicz1(){
+    return x-y;
+}
