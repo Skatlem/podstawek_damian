dcff9f6b2d99571ce7fa6425c8e2a247fc0793d2
Skatlem <115564126+Skatlem@users.noreply.github.com>
2023-02-01 13:19:36 +0100

    ErrorHandle

---
+++ ErrorHandle.h
@@ -0,0 +1,18 @@
+#ifndef __ERROR_HANDLE_H__
+#define __ERROR_HANDLE_H__
+
+#include <iostream>
+
+//! \brief Klasa ERROR HANDLE.
+//!
+//!
+
+class ErrorHandle{
+public:
+    ErrorHandle();
+    ~ErrorHandle();
+private:
+
+}
+
+#endif
